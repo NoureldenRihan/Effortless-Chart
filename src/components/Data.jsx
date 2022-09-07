cb2a526feccda3498d0f3d1b8877b6f877d09112
Nourelden Rihan <noureldenrihan@gmail.com>
2022-09-07 02:27:54 +0200

    UI and Styling Improvements

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -121,6 +121,29 @@ function Data() {
         </div>
         <hr className="breaker" />
         <h2>Data Sets</h2>
+        <div className="datasetManipulation">
+          <svg
+            className="removeDataset"
+            onClick={removeDataset}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z" />
+          </svg>
+          <svg
+            className="addDataset"
+            onClick={addDataset}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
+          </svg>
+        </div>
+
         <div id="datasets" className="datasets">
           <div className="dataset">
             <div className="half">
@@ -141,26 +164,6 @@ function Data() {
                 required
               />
             </div>
-            <svg
-              className="removeDataset"
-              onClick={removeDataset}
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z" />
-            </svg>
-            <svg
-              className="addDataset"
-              onClick={addDataset}
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
-            </svg>
           </div>
           {dataset}
         </div>
